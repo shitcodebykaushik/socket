65cf7f46fe4d7fc800c0ac8f4a41ba60f56c2a9b
kaushik raj <algorithmunloack@gmail.com>
2024-04-11 14:56:53 +0530

    The socket remote tunnel has been intilized properly

---
+++ target/debug/deps/socket.d
@@ -0,0 +1,5 @@
+C:\Users\kaushikraj\Desktop\socket\target\debug\deps\socket.exe: src\main.rs
+
+C:\Users\kaushikraj\Desktop\socket\target\debug\deps\socket.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/socket.d
@@ -0,0 +1,1 @@
+C:\Users\kaushikraj\Desktop\socket\target\debug\socket.exe: C:\Users\kaushikraj\Desktop\socket\src\main.rs
